6bbae2538fe0d57c3ea2dbc3b6817dab36080dac
bfpulliam <bfpulliam@gmail.com>
2019-02-20 17:22:43 -0600

    Yay!  It works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,15 @@ $(document).ready(function () {
 
     //Display random number to be guessed//
     $("#number-to-guess").text(targetNumber);
-    var inputTotal = 0
+    var inputTotal = 0;
+    var wins = 0;
+    var losses = 0;
+
+    function reset(){
+        targetNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+        $("#number-to-guess").text(targetNumber);
+        inputTotal = 0;
+    };
 
 
     $(".gems").on({
@@ -14,7 +22,27 @@ $(document).ready(function () {
             inputTotal += crystalValue;
             var getnum = inputTotal;
              $("#counter").text(getnum);
+            updateCounter();
         }
+        
     });
+
+    function updateCounter() {
+        
+        if (inputTotal === targetNumber) {
+            wins ++;
+          alert("You win!");
+          $("#win").text(wins);
+          reset();
+        }
+        else if (inputTotal > targetNumber) {
+          losses ++;
+          alert("Nope! That's not it!!")
+          $("#loss").text(losses);
+        reset();
+        }
+    }
+    
+
 
 });
